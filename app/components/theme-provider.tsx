d0bcb6c10fa58b7df59942686ec1979166b57abc
Johan Hanses <johanhanses@gmail.com>
2023-05-02 07:38:11 +0200

    clean up provider and update readme

--- app/components/theme-provider.tsx
+++ app/components/theme-provider.tsx
@@ -2,13 +2,14 @@ import type { Dispatch, SetStateAction } from 'react'
 import { createContext, useContext, useEffect, useState } from 'react'
 
 export type Theme = 'light' | 'dark'
-type ThemeContextType = [Theme | null, Dispatch<SetStateAction<Theme | null>>]
+type ThemeContextType = [Theme, Dispatch<SetStateAction<Theme>>]
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState<Theme | null>('dark')
+  const [theme, setTheme] = useState<Theme>('dark')
 
+  // For now this will cause a flash of dark theme on first load is users prefered theme is light
   useEffect(() => {
     const preferedTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
       ? 'dark'
